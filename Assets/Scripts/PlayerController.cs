613f6391fff32a3d3de5461f9807c2366ed80858
Heimdyll <heimdyll@outlook.com>
2017-02-23 10:57:17 -0700

    Got basic jumping implemented -- needs polish

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
-
-    public float minJumpHeight;
-    public float jumpHight;
+    
     public bool isThrowing;
+    public bool isLifting;
+    public float jumpMagnitude;
 
     float speed = 10f;
 	float RotateSpeed = 2f;
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour {
     private Vector3 cameraRotation;
     private GameObject pickedupParent;
     float jumpPressure;
-    bool grounded;
+    public bool grounded = true;
 
     // Use this for initialization
     void Start () {
@@ -26,39 +26,42 @@ public class PlayerController : MonoBehaviour {
         rb3d = GetComponent<Rigidbody>();
         Debug.Log("hand_L?: " + transform.GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(1).GetChild(0).GetChild(0).GetChild(0).GetChild(0).gameObject.name);
     }
-	
-	// Update is called once per frame
-	void Update () {
-		float xMov = Input.GetAxisRaw("Horizontal");
-		float zMov = Input.GetAxisRaw("Vertical");
-		Vector3 MovHorizontal = transform.right*xMov;
-		Vector3 MovVertical = transform.forward*zMov;
-		velocity = (MovHorizontal+MovVertical).normalized*speed;
-		
-		float yRot = Input.GetAxisRaw("Mouse X");
-		rotation = new Vector3(0f, yRot, 0f) *RotateSpeed;
-		
-		float xRot = Input.GetAxisRaw("Mouse Y");
-		cameraRotation = new Vector3(xRot,0f,0f)*RotateSpeed;
-		
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+        Vector3 MovHorizontal = transform.right * xMov;
+        Vector3 MovVertical = transform.forward * zMov;
+        velocity = (MovHorizontal + MovVertical).normalized * speed;
+
+        float yRot = Input.GetAxisRaw("Mouse X");
+        rotation = new Vector3(0f, yRot, 0f) * RotateSpeed;
+
+        float xRot = Input.GetAxisRaw("Mouse Y");
+        cameraRotation = new Vector3(xRot, 0f, 0f) * RotateSpeed;
+
 
         if (!isThrowing && (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0))
         {
             anim.SetBool("isWalking", true);
-        } else
+        }
+        else
         {
             anim.SetBool("isWalking", false);
         }
 
-      
+
 
         if (Input.GetKeyDown(KeyCode.J) && !anim.GetBool("isLifting"))
         {
             DisableMovement(0);
+            //DisableThrow(0);
             anim.SetTrigger("pickupObject");
             anim.SetBool("isLifting", true);
         }
-        else if (Input.GetKeyDown(KeyCode.K) && anim.GetBool("isLifting"))
+        else if (Input.GetKeyDown(KeyCode.K) && anim.GetBool("isLifting") && !isLifting)
         {
             anim.SetTrigger("throwObject");
             anim.SetBool("isLifting", false);
@@ -67,41 +70,50 @@ public class PlayerController : MonoBehaviour {
         /*
         if (grounded == true)
         {*/
-            if (Input.GetButton("Jump"))
+
+        if (Physics.Raycast(transform.position, Vector3.down, 0.1f))
+        {
+            grounded = true;
+            anim.SetBool("isGrounded", true);
+        }
+        else
+        {
+            grounded = false;
+
+
+        }
+        if (Input.GetButton("Jump") && anim.GetBool("isGrounded"))
+        {
+            grounded = false;
+            //anim.SetBool("isGrounded", false);
+            Debug.Log(anim.GetBool("isGrounded"));
+
+            anim.SetBool("isGrounded", false);
+            
+            /*if (jumpPressure < jumpHight)
             {
-                if (jumpPressure < jumpHight)
-                {
-                    jumpPressure += Time.deltaTime * 10f;
-                }
-                else
-                {
-                    jumpPressure = jumpHight;
-                }
+                jumpPressure += Time.deltaTime * 10f;
             }
             else
             {
-                if (jumpPressure > 0f)
-                {
-                    jumpPressure = jumpPressure + minJumpHeight;
-                    GetComponent<Rigidbody>().velocity = Vector3.up * jumpPressure;
-                    jumpPressure = 0f;
-                    grounded = false;
-                }
+                jumpPressure = jumpHight;
+            }*/
+            if (false)//jumpPressure > 0f)
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * 1f, ForceMode.Impulse);
+                jumpPressure = 0f;
+                grounded = false;
+                anim.SetBool("isGrounded", false);
             }
         }
 
+        
+    }
         //Vector3 Floor = transform.TransformDirection(Vector3.down);
 
         //RaycastHit hit;
     /*
-    if (Physics.Raycast(transform.position, Floor, 0.1f))
-    {
-        grounded = true;
-    }
-    else
-    {
-        grounded = false;
-    }
+    
 }*/
     void FixedUpdate()
     {
@@ -123,6 +135,7 @@ public class PlayerController : MonoBehaviour {
         {
             Debug.Log("I'm pickupable");
             pickedupParent = col.transform.parent.gameObject;
+            pickedupParent.transform.position += new Vector3(0, -5, 0);
             col.gameObject.GetComponent<BoxCollider>().enabled = false;
             //col.gameObject.GetComponent<Rigidbody>().useGravity = false;
             col.gameObject.transform.SetParent(transform.GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0));
@@ -134,15 +147,34 @@ public class PlayerController : MonoBehaviour {
     
     public void ThrowObject(int eventInt)
     {
+        transform.GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetComponent<BoxCollider>().enabled = true;
         pickedupParent.transform.position = transform.GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0).position;
         pickedupParent.transform.rotation = transform.GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0).rotation;
         //Debug.Break();
         transform.GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(2).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0).SetParent(pickedupParent.transform);
         pickedupParent.transform.GetChild(0).localEulerAngles = Vector3.zero;
+        pickedupParent.transform.GetChild(0).localScale = Vector3.one;
         //Debug.Break();
         pickedupParent.GetComponent<Rigidbody>().AddForce(transform.forward*100, ForceMode.Impulse);
         //pickedupParent.transform.GetChild(0).localEulerAngles.Set(0, 0, 0);
     }
+
+    public void ActuallyJump(int eventInt)
+    {
+        if (!false)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpMagnitude, ForceMode.Impulse);
+        }
+    }
+    public void DisableThrow(int eventIn)
+    {
+        isLifting = true;
+    }
+    public void EnableThrow(int eventInt)
+    {
+        isLifting = false;
+    }
+
     public void CheckForPickup(int eventInt)
     {
         transform.GetChild(0).gameObject.GetComponent<CapsuleCollider>().enabled = true;
